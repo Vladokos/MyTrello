3b301f039626d9a59a09598c9bb5f5bf81f8bcd9
Vlad <66181660+Vladokos@users.noreply.github.com>
2021-12-15 22:00:58 +0400

    added the list slice in the store

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import boardsReducer from "../features/boards/boardsSlice";
+import listsSlice from '../features/lists/listsSlice';
 
 export const store = configureStore({
   reducer: {
     boards: boardsReducer,
+    lists: listsSlice,
   },
 });
