ce59cbce381978c398245ef95137962770afc3c6
Vlad <66181660+Vladokos@users.noreply.github.com>
2021-12-30 23:33:01 +0400

    added a card reducer

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -2,10 +2,12 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import boardsReducer from "../features/boards/boardsSlice";
 import listsSlice from '../features/lists/listsSlice';
+import cardsSlice from "../features/card/cardsSlice";
 
 export const store = configureStore({
   reducer: {
     boards: boardsReducer,
     lists: listsSlice,
+    cards: cardsSlice,
   },
 });
---
+++ client/src/features/card/cardsSlice.js
@@ -0,0 +1,84 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+import axios from "axios";
+
+const initialState = {
+  cards: [],
+  status: "idle",
+};
+
+export const getCards = createAsyncThunk("cards/getCards", async (idBoard) => {
+  const response = await axios({
+    config: {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    },
+    method: "POST",
+    url: "/board/list/card/get",
+    data: {
+      idBoard,
+    },
+  })
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  return response;
+});
+
+export const addCard = createAsyncThunk(
+  "cards/addCard",
+  async ({ nameCard, idBoard, idList }) => {
+    const response = await axios({
+      config: {
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      },
+      method: "POST",
+      url: "/board/list/card/create",
+      data: {
+        nameCard,
+        idBoard,
+        idList,
+      },
+    })
+      .then((response) => {
+        return response.data;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    return response;
+  }
+);
+
+const cardsSlice = createSlice({
+  name: "cards",
+  initialState,
+  reducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(getCards.pending, (state, action) => {
+        state.status = "loading";
+      })
+      .addCase(getCards.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.cards = state.cards.concat(action.payload);
+      })
+      .addCase(addCard.pending, (state, action) => {
+        state.status = "loading";
+      })
+      .addCase(addCard.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.cards.push(action.payload);
+      });
+  },
+});
+
+export default cardsSlice.reducer;
