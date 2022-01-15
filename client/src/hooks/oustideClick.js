843303754566d70925c3498f67b0d812e5df6193
Vlad <66181660+Vladokos@users.noreply.github.com>
2022-01-15 23:47:09 +0400

    Added function to hide the popup window

--- client/src/hooks/oustideClick.js
+++ client/src/hooks/oustideClick.js
@@ -3,16 +3,12 @@ import { useEffect } from "react";
 export default function OutsideClick(ref, func) {
   useEffect(() => {
     function handleClickOutside(event) {
-      // console.log(ref.current.className);
       if (
         ref.current &&
         !ref.current.contains(event.target) &&
         ref.current.className !== "hidden"
       ) {
-        const asd = () => func();
-        asd();
-        
-        // ref.current.className = "hidden";
+        func();
       }
     }
     document.addEventListener("mousedown", handleClickOutside);
