27d2fc3145083ce65355ce0d572c914be636911d
Vlad <66181660+Vladokos@users.noreply.github.com>
2022-01-28 21:54:08 +0400

    New way to render list also added some dnd

--- client/src/components/board/List.js
+++ client/src/components/board/List.js
@@ -1,28 +1,40 @@
 import React from "react";
 
+import { Droppable } from "react-beautiful-dnd";
+
 import { Card } from "./Card";
 
-export const List = ({ lists, cards, boardId, visibleCardCreate }) => {
+export const List = ({
+  listId,
+  listName,
+  listCards,
+  cards,
+  boardId,
+  visibleCardCreate,
+}) => {
   return (
-    <ul>
-      {lists.length > 0
-        ? lists.map((list) => {
-            if (list.boardId === boardId) {
-              return (
-                <li key={list._id} className={"list " + list.nameList}>
-                  <div className="list-title">{list.nameList}</div>
-                  <Card list={list} cards={cards} />
-                  <button
-                    onClick={(e) => visibleCardCreate(e)}
-                    className={list._id}
-                  >
-                    Add a card
-                  </button>
-                </li>
-              );
-            }
-          })
-        : null}
-    </ul>
+    <Droppable droppableId={listId} direction="horizontal">
+      {(provided) => (
+        <ul {...provided.droppableProps} ref={provided.innerRef}>
+          {/* { if(list.boardId === boardId) { */}
+
+          <li key={listId} className={"list " + listName}>
+            <div className="list-title">{listName}</div>
+            {cards.map((card, index) => {
+              if (listCards.includes(card._id))
+                return <Card card={card} index={index} />;
+            })}
+
+            <button onClick={(e) => visibleCardCreate(e)} className={listId}>
+              Add a card
+            </button>
+          </li>
+
+          {/* }} */}
+
+          {provided.placeholder}
+        </ul>
+      )}
+    </Droppable>
   );
 };
