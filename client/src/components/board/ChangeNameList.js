1bd9899b018110db38dc7f4079fb073eed698816
Vlad <66181660+Vladokos@users.noreply.github.com>
2022-02-04 23:03:39 +0400

    Change name list functional

--- client/src/components/board/ChangeNameList.js
+++ client/src/components/board/ChangeNameList.js
@@ -1,4 +1,7 @@
-import React, { useRef } from "react";
+import React, { useState, useRef } from "react";
+
+import { useDispatch } from "react-redux";
+import { changeName } from "../../features/lists/listsSlice";
 
 import OutsideClick from "../../hooks/outsideClick";
 
@@ -11,22 +14,30 @@ export const ChangeNameList = ({
   listFormShow,
   closeForm,
 }) => {
-  xPos -= 135;
-  yPos -= 132;
+  const dispatch = useDispatch();
+
+  const sendForm = (e) => {
+    if (e.key === "Enter" || e.keyCode === 13) {
+      dispatch(changeName({ listId, nameList })).then(() => closeForm());
+    }
+  };
+
+  const input = useRef(null);
+  OutsideClick(input, () => closeForm());
+
+  xPos -= 138.5;
+  yPos -= 196;
   const styles = {
     position: "absolute",
     transform: `translate(${xPos}px, ${yPos}px)`,
   };
-
-  console.log(listId)
-  const input = useRef(null);
-//   OutsideClick(input, () => {console.log(listId)});
   if (!listFormShow) return null;
   return (
     <div style={styles} className="changeNameList">
       <input
         value={nameList}
         onChange={(e) => changeNameList(e.target.value)}
+        onKeyDown={sendForm}
         ref={input}
       />
     </div>
