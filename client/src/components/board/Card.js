3ababb8b4c45ed7782f6dc96303aaef3d46f97c8
Vlad <66181660+Vladokos@users.noreply.github.com>
2022-02-16 22:53:08 +0400

    added description img

--- client/src/components/board/Card.js
+++ client/src/components/board/Card.js
@@ -2,6 +2,8 @@ import React from "react";
 
 import { Draggable } from "react-beautiful-dnd";
 
+import descriptionImg from "../../img/description.svg";
+
 export const Card = ({ card, index, visibleChangeNameCard }) => {
   return (
     <Draggable
@@ -17,8 +19,19 @@ export const Card = ({ card, index, visibleChangeNameCard }) => {
           {...provided.dragHandleProps}
         >
           <div className="cards">
-            <div className="card" key={card._id} onClick={(e) => visibleChangeNameCard(e, card._id)}>
+            <div
+              className="card"
+              key={card._id}
+              onClick={(e) =>
+                visibleChangeNameCard(e, card._id, card.descriptionCard)
+              }
+            >
               {card.nameCard}
+              {card.descriptionCard.length !== 0 ? (
+                <div>
+                  <img src={descriptionImg} />
+                </div>
+              ) : null}
             </div>
           </div>
         </div>
