8fcbf4b147dc62e950009e2c5915366e097e78fc
Vlad <66181660+Vladokos@users.noreply.github.com>
2022-02-21 20:52:38 +0400

    Added blur send form

--- client/src/components/board/ChangeCard.js
+++ client/src/components/board/ChangeCard.js
@@ -18,22 +18,14 @@ export const ChangeCard = ({
 }) => {
   const dispatch = useDispatch();
 
-  const form = useRef(null);
-  OutsideClick(form, () => closeForm());
-
   const [description, setDescription] = useState("");
 
-  useEffect(() => {
-    setDescription(descriptionCard);
-  }, [descriptionCard]);
-
-  if (!isOpen) return null;
-
   const sendForm = (e) => {
-    if (e.key === "Enter" || e.keyCode === 13) {
+    if (e.type == "blur" || e.key === "Enter" || e.keyCode === 13) {
       switch (e.target.id) {
         case "name":
           dispatch(changeName({ cardId, nameCard }));
+          nameInput.current.blur();
           break;
         case "description":
           dispatch(changeDescription({ cardId, description }));
@@ -44,24 +36,43 @@ export const ChangeCard = ({
     }
   };
 
+  useEffect(() => {
+    setDescription(descriptionCard);
+  }, [descriptionCard]);
+
+  const nameInput = useRef(false);
+  
+  const form = useRef(null);
+  OutsideClick(form, () => closeForm());
+
+  if (!isOpen) return null;
   return (
     <div>
       <div className="blackBG" style={{ left: 0 }}>
-        <div className="changeCardName" ref={form}>
-          <div className="changeCardName-title">Title</div>
+        <div className="changeCard" ref={form}>
+          <div className="changeCard-title">Title</div>
           <TextareaAutosize
             id="name"
             value={nameCard}
             onChange={changeNameCard}
             onKeyDown={sendForm}
+            onBlur={(e) => {
+              sendForm(e);
+            }}
+            spellCheck="false"
+            ref={nameInput}
           />
           <div className="description">Description</div>
+
           <TextareaAutosize
             id="description"
             placeholder="Add a more detail description..."
             value={description}
             onChange={(e) => setDescription(e.target.value)}
-            onKeyDown={sendForm}
+            onBlur={(e) => {
+              sendForm(e);
+            }}
+            spellCheck="false"
           />
           <button onClick={closeForm}>X</button>
         </div>
