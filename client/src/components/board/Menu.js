dccb5a7cb6cdd8001b79a121a7ee0be7301a5d84
Vlad <66181660+Vladokos@users.noreply.github.com>
2022-03-07 22:19:30 +0400

    Menu where a user can see an archived cards and list

--- client/src/components/board/Menu.js
+++ client/src/components/board/Menu.js
@@ -1,10 +1,61 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 
-export const Menu = () => {
+import { ArchiveList } from "./ArchiveList";
+import { ArchiveCard } from "./ArchiveCard";
 
-    return(
+import OutsideClick from "../../hooks/outsideClick";
+
+import archive from "../../img/archive.svg";
+
+import "../../styles/Board/Menu.css";
+
+export const Menu = ({ height, lists, cards }) => {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [archiveListOpen, setArchiveListOpen] = useState(false);
+  const [archiveCardOpen, setArchiveCardOpen] = useState(false);
+
+  const menu = useRef(null);
+  OutsideClick(menu, () => setMenuOpen(false));
+  return (
+    <div>
+      <button className="menu-button" onClick={() => setMenuOpen(true)}>
+        Menu
+      </button>
+      <div
+        className={menuOpen === false ? "hidden" : "menu"}
+        ref={menu}
+        style={{ height: height }}
+      >
         <div>
-           <button>Menu</button>
+          <div className="menuTitle">
+            Menu
+            <button onClick={() => setMenuOpen(false)}>X</button>
+          </div>
+          <hr />
+          <div onClick={() => setArchiveListOpen(true)}>
+            Archived lists <img src={archive} />
+          </div>
+          <div onClick={() => setArchiveCardOpen(true)}>
+            Archived cards <img src={archive} />
+          </div>
         </div>
-    )
-}
+        {archiveListOpen === true ? (
+          <ArchiveList
+            height={height}
+            back={() => setArchiveListOpen(false)}
+            close={() => setMenuOpen(false)}
+            lists={lists}
+          />
+        ) : null}
+        {archiveCardOpen === true ? (
+          <ArchiveCard
+            height={height}
+            back={() => setArchiveCardOpen(false)}
+            close={() => setMenuOpen(false)}
+            cards={cards}
+          />
+        ) : null}
+      </div>
+    </div>
+  );
+};
