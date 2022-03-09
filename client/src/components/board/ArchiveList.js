467815d8964d7502e1bba0d3000b56fca4090d5c
Vlad <66181660+Vladokos@users.noreply.github.com>
2022-03-09 22:52:47 +0400

    added dispatch to unarchive and delete a list

--- client/src/components/board/ArchiveList.js
+++ client/src/components/board/ArchiveList.js
@@ -1,11 +1,20 @@
 import React from "react";
 
+import { useDispatch } from "react-redux";
+
+import { unarchiveList, deleteList } from "../../features/lists/listsSlice";
+
 import recycling from "../../img/recycling.svg";
 import restore from "../../img/restore.svg";
 
 import "../../styles/Board/ArchiveList.css";
 
 export const ArchiveList = ({ height, back, close, lists }) => {
+  const dispatch = useDispatch();
+
+  const unarchive = (listId) => dispatch(unarchiveList({ listId }));
+  const deleting = (listId) => dispatch(deleteList({ listId }));
+
   return (
     <div className="archive" style={{ height: height - 90 }}>
       <div className="archiveTitle">
@@ -21,8 +30,8 @@ export const ArchiveList = ({ height, back, close, lists }) => {
             return (
               <li key={list._id}>
                 {list.nameList}
-                <img src={restore} />
-                <img src={recycling} />
+                <img src={restore} onClick={() => unarchive(list._id)} />
+                <img src={recycling} onClick={() => deleting(list._id)} />
               </li>
             );
           }
