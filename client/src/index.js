4e419c249b4c2d90a66f0a695aff2935a5981d81
Vlad <66181660+Vladokos@users.noreply.github.com>
2022-04-23 00:25:42 +0400

    import socket

--- client/src/index.js
+++ client/src/index.js
@@ -4,10 +4,17 @@ import App from "./App";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
 
+import { io } from "socket.io-client";
+
+const socket = io("http://localhost:3000");
+socket.on("connect", () => {
+  console.log(socket.id); 
+});
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <App socket={socket}/>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
