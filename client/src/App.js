1280d1cdc70d2fcd7e1fbd0422505d60918455fd
Vlad <66181660+Vladokos@users.noreply.github.com>
2022-06-14 20:57:56 +0400

    added auto height

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ import { ResetForm } from "./components/form/ResetForm";
 import { Boards } from "./components/Boards";
 import { Board } from "./components/board/Board";
 
-import {ValidateInvite} from "./components/ValidateInvite";
+import { ValidateInvite } from "./components/ValidateInvite";
 
 import { Profile } from "./components/profile/Profile";
 
@@ -20,22 +20,34 @@ import "./styles/blanks.css";
 import "./styles/formStyles.css";
 import "./styles/Boards.css";
 
+import useWindowHeight from "./hooks/heightWindowHook";
 function App({ socket }) {
+  const { height } = useWindowHeight();
+
   return (
     <BrowserRouter>
       <div className="App">
         <Routes>
-          <Route path="/sig" element={<SignInFrom socket={socket} />} />
-          <Route path="/reg" element={<RegistrationForm socket={socket} />} />
-          <Route path="/forg" element={<ForgotForm socket={socket} />} />
+          <Route
+            path="/sig"
+            element={<SignInFrom socket={socket} height={height} />}
+          />
+          <Route
+            path="/reg"
+            element={<RegistrationForm socket={socket} height={height} />}
+          />
+          <Route
+            path="/forg"
+            element={<ForgotForm socket={socket} height={height} />}
+          />
           <Route
             path="/:token/reset/"
-            element={<ResetForm socket={socket} />}
+            element={<ResetForm socket={socket} height={height} />}
           />
 
           <Route
             path="/:userName/boards"
-            element={<Boards socket={socket} />}
+            element={<Boards socket={socket} height={height}/>}
           />
           <Route
             path="/board/:boardId/:name"
